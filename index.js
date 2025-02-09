fe5eac316cfe1b0af97ec0fa1fb410a81c2938b6
edkim24 <166872556+edkim24@users.noreply.github.com>
2025-02-09 07:23:49 +0300

    create a grid using loops started

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const newDiv = document.createElement('div');
+
+const divContainer = document.querySelector('.grid-container');
+
+// newDiv.setAttribute('class', 'child-div');
+
+//we loop over and create divs, setting an attribute of either a row or column
+
+for (let i = 0; i < 16; i++) {}
