4a4aca77fd4a3be033cca99d1a23c0cf33d76808
edkim24 <166872556+edkim24@users.noreply.github.com>
2025-02-11 03:41:10 +0300

    Squashed commit of the following:
    commit d214daebec2c17f81aebae66192a58237ac4e597
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 03:37:17 2025 +0300
    
        refactor code next
    
    commit 6413949eb27470b5544d5d8dc7ae31cbd4ae35ae
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 03:21:55 2025 +0300
    
        all objectives completed
    
    commit a040bea6e5ebd48d363329ae9f15925e93e1d388
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 03:03:10 2025 +0300
    
        refactor to turn grid creation into a function
    
    commit 6c8a0a4139051da0c86304228a891d04ad97ad53
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 02:53:28 2025 +0300
    
        added control for colors changing and opacity
    
    commit 220dd24259d130f6d365740c15f308e5083cf69b
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 00:52:59 2025 +0300
    
        refactor to functions
    
    commit 899faee6f72c57adc69b291e358bf05764fdf9f9
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Tue Feb 11 00:33:00 2025 +0300
    
        iter 1
    
    commit fe5eac316cfe1b0af97ec0fa1fb410a81c2938b6
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Sun Feb 9 07:23:49 2025 +0300
    
        create a grid using loops started
    
    commit 38f17d08d3e628b66de7e6afad0bb3fca3ff9a2a
    Author: edkim24 <166872556+edkim24@users.noreply.github.com>
    Date:   Sun Feb 9 07:07:33 2025 +0300
    
        start etch a sketch added boiler plate

---
+++ index.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const divContainer = document.querySelector('.grid-container');
+
+//we loop over and create divs, setting an attribute of either a row or column
+
+/*
+
+1.create a 16 by 16 grid, 
+ each square will have dimensions of 50px = 16*50 = 800 by 800
+  - this sets the max grid width as a constant = 800
+2. prompt user input - get number of squares to use 
+3. create the new grid with the adjusted width and height to fit 
+into the max grid width 
+
+
+*/
+
+//refactor code
+
+// const createGrid = function (number = 16, size = 50) {
+// 	for (let i = 0; i < number; i++) {
+// 		const rowDiv = document.createElement('div');
+// 		rowDiv.setAttribute('id', `row-${i}`);
+// 		rowDiv.style.display = 'flex';
+// 		rowDiv.style.maxWidth = 'fit-content';
+// 		divContainer.appendChild(rowDiv);
+// 		for (let i = 0; i < number; ++i) {
+// 			const cellDiv = document.createElement('div');
+// 			cellDiv.setAttribute('id', `cell-${i}`);
+// 			cellDiv.style.width = `${size}px`;
+// 			cellDiv.style.height = `${size}px`;
+// 			cellDiv.classList.add(`row-${i}`);
+// 			cellDiv.classList.add(`cell`);
+// 			rowDiv.appendChild(cellDiv);
+// 		}
+// 		//query to find out max width of grid
+// 	}
+// };
+
+// createGrid();
+
+// we create a 16 by 16 grid
+let maxWidth;
+
+for (let i = 0; i < 16; i++) {
+	const rowDiv = document.createElement('div');
+	rowDiv.setAttribute('id', `row-${i}`);
+	rowDiv.style.display = 'flex';
+	rowDiv.style.maxWidth = 'fit-content';
+	divContainer.appendChild(rowDiv);
+	for (let i = 0; i < 16; ++i) {
+		const cellDiv = document.createElement('div');
+		cellDiv.setAttribute('id', `cell-${i}`);
+		cellDiv.style.width = '40px';
+		cellDiv.style.height = '40px';
+		cellDiv.classList.add(`row-${i}`);
+		cellDiv.classList.add(`cell`);
+		rowDiv.appendChild(cellDiv);
+	}
+	//query to find out max width of grid
+}
+// // we set the max width and height of the container to the output of the create grid
+
+divContainer.style.maxWidth = 'fit-content';
+divContainer.style.maxHeight = 'fit-content';
+
+maxWidth = +getComputedStyle(divContainer).width.slice(0, 3);
+
+divContainer.style.width = maxWidth;
+divContainer.style.height = maxWidth;
+
+// console.log(maxWidth);
+
+// // we get the computed max value for use in our other grids
+
+// // event handler to change color
+// // we tie it to the entire grid
+
+const getRandomValue = function () {
+	return Math.floor(Math.random() * 255) + 1;
+};
+
+const getRandomColor = function () {
+	return `rgb(${getRandomValue()},${getRandomValue()},${getRandomValue()})`;
+};
+
+// // console.log(randomColor);
+
+let opacity = 0;
+
+divContainer.addEventListener('mouseover', e => {
+	e.preventDefault();
+	e.target.style.backgroundColor = `${getRandomColor()}`;
+	opacity === 100 ? (opacity = 10) : (opacity += 10);
+	e.target.style.opacity = `${opacity}%`;
+});
+
+// //popup button event handler
+
+const popupBtn = document.querySelector('button');
+
+popupBtn.addEventListener('click', e => {
+	e.preventDefault();
+	let gridPrompt = 0;
+	do {
+		gridPrompt = +prompt('please enter a value between 1 and 100');
+	} while (gridPrompt <= 0 || gridPrompt > 100);
+
+	// delete previous grid contents
+	divContainer.innerHTML = '';
+	divContainer.maxWidth = divContainer.maxHeight = maxWidth;
+
+	for (let i = 0; i < gridPrompt; i++) {
+		const rowDiv = document.createElement('div');
+		rowDiv.setAttribute('id', `row-${i}`);
+		rowDiv.style.display = 'flex';
+		rowDiv.style.maxWidth = 'fit-content';
+		divContainer.appendChild(rowDiv);
+		for (let i = 0; i < gridPrompt; ++i) {
+			const cellDiv = document.createElement('div');
+			cellDiv.setAttribute('id', `cell-${i}`);
+
+			cellDiv.style.width = cellDiv.style.height = `${
+				(maxWidth - (maxWidth % gridPrompt)) /
+				gridPrompt
+			}px`;
+			cellDiv.classList.add(`row-${i}`);
+			cellDiv.classList.add(`cell`);
+			rowDiv.appendChild(cellDiv);
+		}
+		//query to find out max width of grid
+	}
+});
+
+// console.log(maxWidth);
+
+// //next we need to refactor
